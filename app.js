98fdba8dd2249dbd0963b10967cfdac1a8f2cd1c
Muh-Sharjeel <71692778+Muh-Sharjeel@users.noreply.github.com>
2022-04-18 04:47:12 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+//use credentials from .env file
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'softmak-be'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
