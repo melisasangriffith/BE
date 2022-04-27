d816e8f12e179770d9238383fe24310bc0592a3f
Abin396 <103961110+Abin396@users.noreply.github.com>
2022-04-27 15:05:58 +0100

    second commit

---
+++ routes/updateEmployee.js
@@ -0,0 +1,29 @@
+const app = require('../app');
+
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'softmak-be'
+});
+
+exports.getProducts = (req, res, next) => {
+  connection.connect(function(err) {
+    let i = 0;
+    let products = [];
+    if (err) throw err;
+    connection.query("UPDATE Employee SET column1 = value1,  WHERE [condition]"+(values), (err, rows, fields) => {
+      if (err) throw err;
+      res.json(Object.assign(products));
+      console.log(products);
+    });
+  });
+}
+
+router.get('/retrieve_products', this.getProducts);
+
+module.exports = router;
